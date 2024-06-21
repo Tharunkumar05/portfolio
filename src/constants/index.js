3bde88597946f9dc23e9e83778e32a0810732b25
Tharunkumar05 <tharunsaravanan2003@gmail.com>
2024-06-21 06:43:41 +0530

    commit

---
+++ src/constants/index.js
@@ -0,0 +1,43 @@
+
+
+export const HERO_CONTENT = `Hello! I'm Tharun Kumar, a dedicated Full Stack Developer with a passion for creating dynamic and responsive web applications using the MERN stack. Whether you're a business looking for a developer or a fellow enthusiast wanting to connect, you're in the right place.`;
+
+export const ABOUT_TEXT = `Hi! I'm Tharun Kumar, a passionate Full Stack Developer specializing in the MERN stack (MongoDB, Express.js, React, and Node.js). I love creating robust, scalable web applications that provide exceptional user experiences. With a background in both front-end and back-end development, I thrive on turning innovative ideas into real-world solutions that make a difference. Whether it's building from scratch or improving existing systems, I'm committed to delivering high-quality, efficient, and maintainable code.`;
+
+
+export const PROJECTS = [
+  {
+    title: "E-Commerce Website",
+    // image: project1,
+    description:
+      "A fully functional e-commerce website with features like product listing, shopping cart, and user authentication.",
+    technologies: ["HTML", "CSS", "React", "Node.js", "MongoDB", "ExpressJs"],
+  },
+  {
+    title: "Dental Clinic Management System",
+    // image: project2,
+    description:
+      "An application for managing appointments and patient data in a secured way and easy to allot the rooms for the patient.",
+    technologies: ["HTML", "CSS", "Django" ],
+  },
+  {
+    title: "Portfolio Website",
+    // image: project3,
+    description:
+      "A personal portfolio website showcasing projects, skills, and contact information.",
+    technologies: ["HTML", "CSS", "React", "Bootstrap"],
+  },
+  {
+    title: "Yelpcamp",
+    // image: project4,
+    description:
+      "A platform for creating and publishing tourist spots posts, with features like  commenting, and user profiles.",
+    technologies: ["HTML", "CSS", "EJS", "Express", "Node.js", "MongoDB"],
+  },
+];
+
+export const CONTACT = {
+  address: "Kongu Engineering College ",
+  phoneNo: "+91 8838933232 ",
+  email: "tharunkumars.21cse@kongu.edu",
+};
